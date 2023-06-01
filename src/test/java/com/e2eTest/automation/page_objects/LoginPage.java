7a4c513407c2c0aefa1dbaa16a97f4fa1baf8b04
ATTAFIYousra <attafi.yosra@hotmail.fr>
2023-06-02 01:09:59 +0200

    delete cachelookup

--- src/test/java/com/e2eTest/automation/page_objects/LoginPage.java
+++ src/test/java/com/e2eTest/automation/page_objects/LoginPage.java
@@ -1,7 +1,6 @@
 package com.e2eTest.automation.page_objects;
 
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
@@ -11,15 +10,12 @@ import com.e2eTest.automation.utils.Setup;
 public class LoginPage extends BasePage {
 
 	/* Retrieve Element */
-	@CacheLookup
 	@FindBy(how = How.ID, using = "Email")
 	private static WebElement email;
 
-	@CacheLookup
 	@FindBy(how = How.ID, using = "Password")
 	private static WebElement password;
 
-	@CacheLookup
 	@FindBy(how = How.XPATH, using = "//button[@type='submit']")
 	private static WebElement btnLogin;
 
